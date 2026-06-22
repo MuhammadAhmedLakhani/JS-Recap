85c24c1fb373f494ade0d919053262216b286109
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2026-06-22 18:58:13 +0500

    var:redeclaration

--- Variables And Declaration/js/app.js
+++ Variables And Declaration/js/app.js
@@ -1,8 +1,18 @@
 
 // Variables And Declaration
 
-var a;  // declare karna
+// var a;  // declare karna
 
-var a = 12; //declare and initialization
+// var a = 12; //declare and initialization
             // initialization is giving first value
+
+
+
+
+var a = 12;
+
+var a  = 122;
+
+console.log(a)        //no error eve on redeclaration
+
 
