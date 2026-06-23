db3f7bb13308d18c55b7b7e62a07c71c71928d82
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2026-06-23 19:27:01 +0500

    Datatype:number

--- Data Types + Type System/js/app.js
+++ Data Types + Type System/js/app.js
@@ -4,20 +4,29 @@
 // when you copy them they copy fully (deep-copy)
 
 
-let a = 12;
-console.log(a)
-let c = a;  // copying them 
- console.log(c)  //same value as of a 
+// let a = 12;
+// console.log(a)
+// let c = a;  // copying them 
+//  console.log(c)  //same value as of a 
  
- a = 40
- console.log(a)  //updated a value 
- console.log(c)  // value of c is not updated 
+//  a = 40
+//  console.log(a)  //updated a value 
+//  console.log(c)  // value of c is not updated 
  
-
-
-
-
-
 // ---------------non-primitive data types
 //array [] object {} functions ()
 //when you copy them they refer to the original one (shallow copy)
+
+
+// ------------------------------------ string
+
+//they contain data which is wrapped in single --> '' or double quotes --> "" or backticks --> ``
+
+
+//--------------------------------------- number
+
+// they contain numerical data 
+
+let a = 12; // number
+
+let b = 12.5 //number
