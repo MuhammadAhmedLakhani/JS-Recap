2b3e0bf4c39ccb519f896a2c0564f58de22e2858
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2026-06-23 18:56:49 +0500

    Datatype:non-primitive

--- Data Types + Type System/js/app.js
+++ Data Types + Type System/js/app.js
@@ -4,3 +4,6 @@
 // when you copy them they copy fully (deep-copy)
 
 
+// ---------------non-primitive data types
+//array [] object {} functions ()
+//when you copy them they refer to the original one (shallow copy)
