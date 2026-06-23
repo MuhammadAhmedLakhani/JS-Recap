fe9e59b89af5d2b96a01154e26f9789dd870492a
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2026-06-23 19:10:14 +0500

    Primitive:copy example

--- Data Types + Type System/js/app.js
+++ Data Types + Type System/js/app.js
@@ -4,6 +4,20 @@
 // when you copy them they copy fully (deep-copy)
 
 
+let a = 12;
+console.log(a)
+let c = a;  // copying them 
+ console.log(c)  //same value as of a 
+ 
+ a = 40
+ console.log(a)  //updated a value 
+ console.log(c)  // value of c is not updated 
+ 
+
+
+
+
+
 // ---------------non-primitive data types
 //array [] object {} functions ()
 //when you copy them they refer to the original one (shallow copy)
