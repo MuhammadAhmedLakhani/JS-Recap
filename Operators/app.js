90cdc7e5f2f87af7c09a313118c2f74f424bf243
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2026-06-29 17:20:18 +0500

    Operators:Modulud

--- Operators/app.js
+++ Operators/app.js
@@ -4,7 +4,7 @@
 
 //+ = / * % **
 
-let a = 10;;
+let a = 11;;
 let b = 2;;
 
 // console.log(a + b)
@@ -12,4 +12,7 @@ let b = 2;;
 // console.log(a / b)
 // console.log(a * b)
 
-console.log(a ** b)    ///Exponential operation 
+// console.log(a ** b)    ///Exponential operation 
+
+
+console.log(a % b)  //mid operator  (it gives remainder after devision)
