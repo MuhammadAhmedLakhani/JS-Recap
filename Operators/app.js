6e3788aafa562cbe5ab6830a52cf02fa8328523c
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2026-06-29 17:14:36 +0500

    Operator:basic airthmatic operators

---
+++ Operators/app.js
@@ -0,0 +1,13 @@
+// ===========================  Operators ===============================
+
+// Aithmatic Operators
+
+//+ = / * % **
+
+let a = 10;;
+let b = 2;;
+
+console.log(a + b)
+console.log(a - b)
+console.log(a / b)
+console.log(a * b)
