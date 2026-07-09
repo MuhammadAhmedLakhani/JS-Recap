f919e7814f10f7b13df071a4e4577c991da39fa0
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2026-07-09 19:39:49 +0500

    Operators:less than sign

--- Operators/app.js
+++ Operators/app.js
@@ -42,7 +42,15 @@ let b = 2;;
 
 // console.log(12 !== "12") // return true as both are "not equal" as condition given (data types make them different in reality)
 
-//">" --> greater than sign
-console.log(12 > 13) // returns false as 12 is not greater than 13
+// //">" --> greater than sign
+// console.log(12 > 13) // returns false as 12 is not greater than 13
 
-console.log(1200 > 1100 ) // returns true as 1200 is lesser than 1100
+// console.log(1200 > 1100 ) // returns true as 1200 is lesser than 1100
+
+
+//"<" --> less than sign
+console.log(12 < 13) // returns true
+
+console.log(13 < 11) // returns false
+
+
